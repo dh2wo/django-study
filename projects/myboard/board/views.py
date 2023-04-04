f33e62aae848586a33e7ab98064fd74c2a37cd53
dh2wo <osj4927@gmail.com>
2023-04-04 09:49:26 +0900

    CBV

--- projects/myboard/board/views.py
+++ projects/myboard/board/views.py
@@ -4,6 +4,8 @@ from django.core.paginator import Paginator
 from django.core import serializers
 from django.contrib.auth.decorators import login_required
 
+from django.views.generic import ListView, DeleteView # CBV
+
 from json import loads # 문자열화한 JSON객체 받을수 있다?
 
 from .models import Board, Reply
@@ -329,3 +331,22 @@ def download(request, id):
     response['Content-Disposition'] = 'attachment; filename = %s' %original_file_name
 
     return response
+
+### Class Based View ###
+class BoardList(ListView):
+    # ListView : django에서 제공하는 목록을 보여주는 View 기능
+    # model = 이 페이지에서 표시할 객체 타입
+    model = Board
+
+    # ordering 속성에는 문자열로 내가 정렬하고 싶은 열 이름을 쓴다
+    # 열 이름 앞에 -가 붙어 있으면 내림차순 정렬
+    ordering = '-id'
+
+    # 클래스 기반 뷰에서 사용하는 템플릿은
+    # 일반적으로 이름이 객체이름_list.html
+
+class BoardDetail(DeleteView):
+    model = Board
+    # template_name 속성 : 내가 별도로 이용하고 싶은 템플릿 파일이 있을 때 해당 파일 이름 지정
+    # template_name을 사용하지 않으면 model이름_detail.html을 찾아간다
+    template_name = 'board/read.html'
